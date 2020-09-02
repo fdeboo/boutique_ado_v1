93a030f0ee86e84294ed50bba5d68eee6c3b7c04
Frances <frances.deboo@gmail.com>
2020-09-02 22:09:02 +0100

    Completed webhook handler

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['d80974d1654a.ngrok.io', '127.0.0.1']
+ALLOWED_HOSTS = ['5a568888d1a8.ngrok.io', '127.0.0.1']
 
 
 # Application definition
