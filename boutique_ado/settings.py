c7bfac88debd8cca2baaed4aa448aba0c3024490
Frances <frances.deboo@gmail.com>
2020-09-08 18:32:55 +0100

    Shopping bag refactored

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,7 +181,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cache Control
-    
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
