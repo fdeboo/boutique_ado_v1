ed3ad526819ec46790586eb1bebc2c86cc506127
Frances <frances.deboo@gmail.com>
2020-09-08 15:50:03 +0100

    Added Cache Control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,6 +182,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 if 'USE_AWS' in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT'
+        'CacheControl': 'max-age=94608000',
+    }
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'fdeboo-boutique-ado'
     AWS_S3_REGION_NAME = 'us-east-1'
