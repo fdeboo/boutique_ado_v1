9c5de6f8d14755c65ebb6845967267bab3423096
Frances <frances.deboo@gmail.com>
2020-09-08 16:29:48 +0100

    comma added to object parameters

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,8 +183,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Cache Control
+    
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT'
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
 
