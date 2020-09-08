af0433f418c7c934bfced117415294dc9470984e
Frances <frances.deboo@gmail.com>
2020-09-08 19:50:18 +0100

    Shopping bag refactored

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['fdeboo-boutique-ado.herokuapp.com', '127.0.0.1']
 
